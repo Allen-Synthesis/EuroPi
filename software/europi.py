1be8fcd051a76044282102dde56526432f8eeff8
Rory Allen <79809962+roryjamesallen@users.noreply.github.com>
2021-11-19 12:29:15 +0000

    Add files via upload
    Change digital_input to din to match analogue input

--- software/europi.py
+++ software/europi.py
@@ -3,6 +3,11 @@ from ssd1306 import SSD1306_I2C
 from time import sleep
 
 
+#Rory Allen 19/11/2021 CC BY-SA 4.0
+#Import this library into your own programs using 'from europi import *'
+#You can then use the inputs, outputs, knobs, and buttons as objects, and make use of the general purpose functions
+
+
 oled = SSD1306_I2C(128, 32, I2C(0, sda=Pin(0), scl=Pin(1), freq=400000))
 oled.fill(0)
 oled.show()
@@ -10,7 +15,7 @@ oled.show()
 button1 = Pin(4, Pin.IN)
 button2 = Pin(5, Pin.IN)
 
-digital_input = Pin(22, Pin.IN)
+din = Pin(22, Pin.IN)
 
 
 class output:
@@ -24,13 +29,10 @@ class output:
     def clamp(self, value):
         return max(min(value, 65534), 0)
         
-    def duty_raw(self, cycle):
+    def duty(self, cycle):
         cycle = int(cycle)
         self.output.duty_u16(self.clamp(cycle))
         self.current_duty = cycle
-        
-    def duty(self, cycle):
-        self.duty_raw(cycle)
         
     def voltage(self, voltage):
         self.duty(voltage * self.output_multiplier)
@@ -39,7 +41,7 @@ class output:
         self.voltage(5)
     
     def off(self):
-        self.duty_raw(0)
+        self.duty(0)
         
 
 def get_output_calibration_data():
@@ -67,12 +69,9 @@ class analogue_input:
     def __init__(self, pin):
         self.input = ADC(Pin(pin))
         self.input_multiplier, self.input_offset = get_input_calibration_data()
-    
-    def read_raw(self, samples=256):
+
+    def read_duty(self, samples=256):
         return sample_adc(self.input, samples)
-    
-    def read_duty(self):
-        return self.read_raw()
     
     def read_voltage(self):
         return (self.read_duty() * self.input_multiplier) + self.input_offset
@@ -82,15 +81,15 @@ class knob:
     def __init__(self, pin):
         self.input = ADC(Pin(pin))
         
-    def read_position(self, steps=100):
-        return round(steps - ((sample_adc(self.input) / 4096) * steps))
+    def read_position(self, steps=100, samples=256):
+        return round(steps - ((sample_adc(self.input, samples) / 4096) * steps))
 
 
-def digital_input_handler(pin): 
-    digital_input.irq(handler=None)
+def din_handler(pin): 
+    din.irq(handler=None)
     #function
-    digital_input.irq(handler=digital_input_handler)
-digital_input.irq(trigger=Pin.IRQ_FALLING, handler=digital_input_handler)
+    din.irq(handler=din_handler)
+din.irq(trigger=Pin.IRQ_FALLING, handler=din_handler)
 
 
 def button_1_handler(pin): 
@@ -136,9 +135,10 @@ def centre_text(text):
 
 
 
+#Calibration program. Run this program to calibrate the module
 if __name__ == '__main__':
     def wait_for_range(low, high):
-        while ain.read_raw() < low or ain.read_raw() > high:
+        while ain.read_duty() < low or ain.read_duty() > high:
             sleep(0.05)
             
     def wait_and_show(low, high):
@@ -158,18 +158,18 @@ if __name__ == '__main__':
     centre_text('Welcome\nto the\ncalibrator')
     oled.show()
     sleep(3)
-    if ain.read_raw() > 100:
+    if ain.read_duty() > 100:
         centre_text('Please unplug\nall patch\ncables')
         oled.show()
     wait_for_range(0, 100)
     centre_text('Plug 1V into\nanalogue input')
     oled.show()
     wait_and_show(low_threshold_low, low_threshold_high)
-    low_reading = ain.read_raw(4096)
+    low_reading = ain.read_duty(4096)
     centre_text('Now plug 10V\ninto analogue\ninput')
     oled.show()
     wait_and_show(high_threshold_low, high_threshold_high)
-    high_reading = ain.read_raw(4096)
+    high_reading = ain.read_duty(4096)
     
     input_multiplier = (HIGH_VOLTAGE - LOW_VOLTAGE) / (high_reading - low_reading)
     input_offset = HIGH_VOLTAGE - (input_multiplier * high_reading)
@@ -179,9 +179,9 @@ if __name__ == '__main__':
     wait_for_range(0, 100)
     centre_text('Plug output 1\ninto analogue\ninput')
     oled.show()
-    cv1.duty_raw(65534)
+    cv1.duty(65534)
     wait_and_show(high_threshold_low, high_threshold_high)
-    output_multiplier = 65534 / ((ain.read_raw(4096) * input_multiplier) + input_offset)
+    output_multiplier = 65534 / ((ain.read_duty(4096) * input_multiplier) + input_offset)
 
     with open('lib/calibration.txt', 'w') as file:
         file.write(str(input_multiplier) + '\n' + str(input_offset) + '\n' + str(output_multiplier))
