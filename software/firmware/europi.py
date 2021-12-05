ba21e6ad62074c5737437a4e053f5dd3bc7c4f76
Rory Allen <79809962+roryjamesallen@users.noreply.github.com>
2021-12-05 17:53:00 +0000

    Add new license

--- software/firmware/europi.py
+++ software/firmware/europi.py
@@ -4,11 +4,26 @@ from time import sleep, ticks_ms
 from sys import exit
 
 
-#Rory Allen 19/11/2021 CC BY-SA 4.0
+#Copyright [yyyy] [name of copyright owner]
+
+#Licensed under the Apache License, Version 2.0 (the "License");
+#you may not use this file except in compliance with the License.
+#You may obtain a copy of the License at
+
+#http://www.apache.org/licenses/LICENSE-2.0
+
+#Unless required by applicable law or agreed to in writing, software
+#distributed under the License is distributed on an "AS IS" BASIS,
+#WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#See the License for the specific language governing permissions and
+#limitations under the License. 
+
+
+
+
 #Import this library into your own programs using 'from europi import *'
 #You can then use the inputs, outputs, knobs, and buttons as objects, and make use of the general purpose functions
 
-        
 #General use functions
 def clamp(value, low, high): #Returns a value that is no lower than 'low' and no higher than 'high'
     return max(min(value, high), low)
