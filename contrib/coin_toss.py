a69f876fed7edb307bad781ad4dee152fb012dba
Adam Wonak <adam.wonak@gmail.com>
2022-01-09 18:55:50 -0600

    Triggers should sleep during the same period, not sequentially.

--- contrib/coin_toss.py
+++ contrib/coin_toss.py
@@ -31,31 +31,19 @@ from utime import sleep_ms, ticks_ms
 import machine
 
 
+# Internal clock tempo range.
 MAX_BPM = 280
 MIN_BPM = 20
 
-# Constant values for display
+# Constant values for display.
 FRAME_WIDTH = int(OLED_WIDTH / 8)
 
-# Number of sequential reads for smoothing analog read values
-SAMPLES = 256
+# Number of sequential reads for smoothing analog read values.
+SAMPLES = 32
 
-# Overclock the Pico
-#machine.freq(250000000)
-machine.freq(125000000)
-
-
-## Override oled with a noop
-class Noop(object):
-    def noop(*args, **kw): pass
-    def __getattr__(self, _): return self.noop
-#oled = Noop()
-
-
-def trigger(output, delay=10):
-    output.on()
-    sleep_ms(delay)
-    output.off()
+# Overclock the Pico for improved performance.
+machine.freq(250000000)
+#machine.freq(125000000)  # Default clock speed.
 
 
 class CoinToss:
@@ -111,12 +99,13 @@ class CoinToss:
         """
         coin = random()
         # Sum the knob2 and analogue input values to determine threshold.
-        self.threshold = clamp(k2.read_position(SAMPLES)/100 + ain.read_voltage(SAMPLES)/12, 0, 1)
+        read_sum = k2.unit_interval(SAMPLES) + ain.read_voltage(SAMPLES)/12
+        self.threshold = clamp(read_sum, 0, 1)
         if self.gate_mode:
             a.value(coin < self.threshold)
             b.value(coin > self.threshold)
         else:
-            trigger(a if coin < self.threshold else b)
+            (a if coin < self.threshold else b).on()
         
         if not draw:
             return
@@ -140,10 +129,18 @@ class CoinToss:
             oled.fill_rect(0, 0, FRAME_WIDTH, OLED_HEIGHT, 0)
 
             self.toss(cv1, cv4)
-            trigger(cv3)
+            cv3.on()  # First column clock trigger
             if counter % 4 == 0:
                 self.toss(cv2, cv5, False)
-                trigger(cv6)
+                cv6.on()  # Second column clock trigger (1/4x speed)
+            
+            sleep_ms(10)
+            if self.gate_mode:
+                # Only turn off clock triggers.
+                [o.off() for o in (cv3, cv6)]
+            else:
+                # Turn of all cvs in trigger mode.
+                [o.off() for o in cvs]
 
             # Draw threshold line
             oled.hline(0, int(self.threshold * OLED_HEIGHT), FRAME_WIDTH, 1)
