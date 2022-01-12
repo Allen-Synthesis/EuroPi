8da91390700c1f521e13b05780749d78f7229f88
Rory Allen <79809962+roryjamesallen@users.noreply.github.com>
2022-01-12 12:31:39 +0000

    Create harmonic_lfos.py

---
+++ contrib/harmonic_lfos.py
@@ -0,0 +1,15 @@
+from europi import *
+from math import sin, radians
+
+MAX_VOLTAGE = 5
+HARMONICS = [1, 2, 3, 5, 7, 11]
+degree = 0
+delay = k1.read_position(100)/10000
+while True:
+    rad = radians(degree)
+    for cv, multiplier in zip(cvs, HARMONICS):
+        cv.voltage(((sin(rad*(1/multiplier)))+1)*(MAX_VOLTAGE/2))
+    degree += 1
+    sleep(delay)
+    if degree % 10 == 0:
+        delay = 0.1 - (k1.read_position(100)/1000)
