7f5b4db8e91cc19389de83024fc3c3143840c6b9
Rory Allen <79809962+roryjamesallen@users.noreply.github.com>
2022-01-12 16:19:23 +0000

    Allow faster LFOs without stepping, add reset function

--- contrib/harmonic_lfos.py
+++ contrib/harmonic_lfos.py
@@ -1,15 +1,28 @@
 from europi import *
 from math import sin, radians
 
+def reset():
+    global degree
+    degree = 0
+din.handler(reset)
+b1.handler(reset)
+
+def get_delay_increment_value():
+    delay = (0.1 - (k1.read_position(100)/1000)) + (ain.read_voltage(1)/100)
+    return delay, round((((1/delay)-10)/1)+1)
+
 MAX_VOLTAGE = 5
 HARMONICS = [1, 2, 3, 5, 7, 11]
 degree = 0
-delay = (0.1 - (k1.read_position(100)/1000)) + (ain.read_voltage(1)/100)
+delay, increment_value = get_delay_increment_value()
 while True:
     rad = radians(degree)
     for cv, multiplier in zip(cvs, HARMONICS):
         cv.voltage(((sin(rad*(1/multiplier)))+1)*(MAX_VOLTAGE/2))
-    degree += 1
+    
+    degree += increment_value
     sleep(delay)
-    if degree % 10 == 0:
-        delay = (0.1 - (k1.read_position(100)/1000)) + (ain.read_voltage(1)/100)
+    
+    if round(degree, -1) % 10 == 0:
+        delay, increment_value = get_delay_increment_value()
+
