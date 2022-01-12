934f54a62e5acf174f31298ddc2e375eed8cf47d
Rory Allen <79809962+roryjamesallen@users.noreply.github.com>
2022-01-12 16:26:22 +0000

    Readability

--- contrib/harmonic_lfos.py
+++ contrib/harmonic_lfos.py
@@ -1,6 +1,11 @@
 from europi import *
 from math import sin, radians
 
+
+MAX_VOLTAGE = 5
+HARMONICS = [1, 3, 5, 7, 11, 13]
+
+
 def reset():
     global degree
     degree = 0
@@ -11,8 +16,6 @@ def get_delay_increment_value():
     delay = (0.1 - (k1.read_position(100)/1000)) + (ain.read_voltage(1)/100)
     return delay, round((((1/delay)-10)/1)+1)
 
-MAX_VOLTAGE = 5
-HARMONICS = [1, 2, 3, 5, 7, 11]
 degree = 0
 delay, increment_value = get_delay_increment_value()
 while True:
