6687e583b7d60b5891fc0a980d511fcb9330f0f4
Rory Allen <rory.james.allen1@gmail.com>
2022-01-22 13:11:21 +0000

    Revert "Fix file write"
    This reverts commit cf141e8d835cf993f60d0cbd5400a6f8883c749a.

--- software/firmware/calibrate.py
+++ software/firmware/calibrate.py
@@ -1,7 +1,6 @@
 from machine import Pin, ADC, PWM
 from time import sleep
 
-machine.freq(250000000)
 
 ain = ADC(Pin(26, Pin.IN, Pin.PULL_DOWN))
 cv1 = PWM(Pin(21))
@@ -13,10 +12,8 @@ def wait_for_voltage(voltage):
     else:
         input('\nPlease unplug all cables and then press Enter')
     print('\n Calibrating...')
-    readings = []
-    for reading in range(256):
-        readings.append(ain.read_u16())
-    return round(sum(readings)/256)
+    sleep(2)
+    return round(ain.read_u16())
 
 
 chosen_process = ''
@@ -45,31 +42,3 @@ else:
 with open(f'lib/calibration.py', 'w') as file:
     values = ", ".join(map(str, readings))
     file.write(f"CALIBRATION_VALUES=[{values}]")
-print(f'\n{readings}\n')
-    
-    
-
-from europi import AnalogueInput
-ain = AnalogueInput(26)
-cv1 = PWM(Pin(21))
-
-input('\nPlease plug CV output 1 into the analogue input and then press Enter')
-output_duties = []
-duty = 0
-reading = 0
-for voltage in range(1,11):
-    while abs(reading - voltage) > 0.005 and reading < voltage:
-        cv1.duty_u16(duty)
-        duty += 25
-        reading = round(ain.read_voltage(),2)
-    output_duties.append(duty)
-    print(f'\n{voltage}V')
-        
-print(f'\n{output_duties}')
-
-with open(f'lib/calibration.py', 'a+') as file:
-    values = ", ".join(map(str, output_duties))
-    file.write(f"\nOUTPUT_CALIBRATION_VALUES=[{values}]")
-    
-print('\nCalibration Complete!')
-
