a08dc5d1668c462f80ef44f581771de557671c5f
Rory Allen <79809962+roryjamesallen@users.noreply.github.com>
2022-01-25 13:20:06 +0000

    Update calibrate.py

--- software/firmware/calibrate.py
+++ software/firmware/calibrate.py
@@ -1,9 +1,8 @@
-from machine import Pin, ADC, PWM, freq
+from machine import Pin, ADC, PWM
 from time import sleep
-from europi import oled, b1, b2, MAX_UINT16
+from europi import oled, b1, b2
 
-# Overclock for faster calibration
-freq(250_000_000)
+machine.freq(250000000)
 
 
 ain = ADC(Pin(26, Pin.IN, Pin.PULL_DOWN))
@@ -14,10 +13,10 @@ usb = Pin(24, Pin.IN)
 def wait_for_voltage(voltage):
     wait_for_b1(0)
     if voltage != 0:
-        oled.centre_text(f'Plug {voltage}V into\nanalogue in\nthen press B1')
+        oled.centre_text(f'Plug in {voltage}V\n\nDone: Button 1')
         wait_for_b1(1)
     else:
-        oled.centre_text(f'Please unplug\nall cables\nthen press B1')
+        oled.centre_text(f'Unplug all\n\nDone: Button 1')
         wait_for_b1(1)
     oled.centre_text('Calibrating...')
     sleep(1.5)
@@ -46,18 +45,14 @@ def wait_for_b1(value):
         sleep(0.05)
 
 
-flash(8, 0.2)
-
-
 if usb.value() == 1:
-    text_wait('Please connect\nrack power only\nnot USB', 4)
-    oled.centre_text('Please connect\nrack power only\nnot USB')
+    oled.centre_text('Make sure rack\npower is on\nDone: Button 1')
+    wait_for_b1(1)
+    wait_for_b1(0)
+    
+text_wait('Calibration\nMode', 3)
 
-text_wait('Welcome to the\ncalibration\nprocess', 3)
-text_wait('There are 2\noptions for\ncalibrating', 3)
-text_wait('1: You need\n a variable\nvoltage source', 3)
-text_wait('2: You only\nneed a\n10V supply', 3)
-text_wait('Use buttons to\nchoose\n1           2', 0)
+oled.centre_text('Choose Process\n\n1         2')
 while True:
     if b1.value() == 1:
         chosen_process = 1
@@ -75,7 +70,7 @@ else:
     for voltage in range(11):
         readings.append(wait_for_voltage(voltage))
         
-with open(f'lib/calibration.py', 'w') as file:
+with open(f'lib/calibration_values.py', 'w') as file:
     values = ", ".join(map(str, readings))
     file.write(f"INPUT_CALIBRATION_VALUES=[{values}]")
     
@@ -84,7 +79,7 @@ from europi import AnalogueInput
 ain = AnalogueInput(26)
 cv1 = PWM(Pin(21))
 
-oled.centre_text(f'Plug CV1 into\nanalogue in\nthen press B1')
+oled.centre_text(f'Plug CV1 into\nanalogue in\nDone: Button 1')
 wait_for_b1(1)
 oled.centre_text('Calibrating...')
 
@@ -92,18 +87,14 @@ output_duties = [0]
 duty = 0
 reading = 0
 for voltage in range(1,11):
-    while abs(reading - voltage) > 0.005 and reading < voltage:
+    while abs(reading - voltage) > 0.002 and reading < voltage:
         cv1.duty_u16(duty)
-        duty += 25
-        if duty > MAX_UINT16:
-            text_wait('Calibration\ncould not\ncomplete', 2)
-            text_wait('Double check\nresistor values\and soldering', 2)
-            break
+        duty += 10
         reading = round(ain.read_voltage(),2)
     output_duties.append(duty)
     oled.centre_text(f'Calibrating...\n{voltage}V')
 
-with open(f'lib/calibration.py', 'a+') as file:
+with open(f'lib/calibration_values.py', 'a+') as file:
     values = ", ".join(map(str, output_duties))
     file.write(f"\nOUTPUT_CALIBRATION_VALUES=[{values}]")
     
