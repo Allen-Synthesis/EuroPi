efd89fa64f056dd7891751fb4642ed86de7be808
Adam Wonak <adam.wonak@gmail.com>
2022-02-13 18:13:45 -0600

    Add the ability to enter diagnostic mode at boot time when both buttons pressed.

--- software/firmware/europi.py
+++ software/firmware/europi.py
@@ -19,6 +19,7 @@ from machine import ADC
 from machine import I2C
 from machine import PWM
 from machine import Pin
+from time import sleep_ms
 
 from ssd1306 import SSD1306_I2C
 
@@ -72,6 +73,31 @@ def reset_state():
         oled.clear()
     [cv.off() for cv in cvs]
     [d.reset_handler() for d in (b1, b2, din)]
+
+
+def diagnostic():
+    """Run the diagnostic script."""
+    # Set the outputs to useful values
+    cv1.voltage(0)  # min
+    cv2.voltage(0.5)  # not 0 but still below DI's threshold
+    cv3.voltage(1)
+    cv4.voltage(4)
+    cv5.voltage(5)
+    cv6.voltage(10)  # max
+
+    while True:
+        oled.fill(0)
+
+        # display the input values
+        oled.text(f"ain: {ain.read_voltage():5.2f}v", 2, 3, 1)
+        oled.text(f"k1: {k1.read_position():2}  k2: {k2.read_position():2}", 2, 13, 1)
+        oled.text(f"din:{din.value()} b1:{b1.value()} b2:{b2.value()}", 2, 23, 1)
+
+        # show the screen boundaries
+        oled.rect(0, 0, OLED_WIDTH, OLED_HEIGHT, 1)
+        oled.show()
+
+        sleep_ms(10)
 
 
 # Component classes.
@@ -431,3 +457,7 @@ cvs = [cv1, cv2, cv3, cv4, cv5, cv6]
 
 # Reset the module state upon import.
 reset_state()
+
+# If both button 1 & 2 are held down at boot, run diagnostic script
+if all([b1.value(), b2.value()]):
+    diagnostic()
