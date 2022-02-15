c834f3acdeaf987bd82a4fc16a21c55b73e10532
Matthew Jaskula <mjaskula@gmail.com>
2022-02-15 11:09:14 -0500

    imporve deploy script
    - allow an optional alternative destination
    - add the -a flag which will attach to the REPL so that you can receive output

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,5 +1,25 @@
 #!/bin/sh
 set -e
 
-rshell cp ${1} /pyboard/main.py
-rshell 'repl ~ import machine ~ machine.soft_reset()~'
+while getopts ":a" opt; do
+    case $opt in
+        a)
+            attach=1
+            ;;
+        \?)
+            echo "Invalid option: -$OPTARG" >&2
+            ;;
+    esac
+done
+
+src=${1}
+dest=${2:-'/pyboard/main.py'}
+
+RESTART_COMMAND='repl ~ import machine ~ machine.soft_reset()'
+
+if [[ $attach -ne 1 ]]; then
+    RESTART_COMMAND="${RESTART_COMMAND}~"
+fi
+
+rshell cp ${src} ${dest}
+rshell $RESTART_COMMAND
