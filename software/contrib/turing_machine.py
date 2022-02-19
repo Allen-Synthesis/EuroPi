b8ea0e3603c74874ec468f061323b44a005704c0
Matthew Jaskula <mjaskula@gmail.com>
2022-02-19 16:16:59 -0500

    notes

---
+++ software/contrib/turing_machine.py
@@ -0,0 +1,20 @@
+"""
+
+din - clock
+ain - cv
+k1 - the big one
+k2 - scale
+b1 - length cycle
+b2 - write
+cv1 - out
+cv2 - noise
+cv3 - pulse
+cv4 - pulse
+cv5 - pulse
+cv6 - pulse
+"""
+
+class TuringMachine():
+    pass
+
+# code to tie it to the EuroPi's interface
