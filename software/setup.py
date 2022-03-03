5c5d72740f9179858f6d02f9b36d2d635aabb0da
Matthew Jaskula <mjaskula@gmail.com>
2022-03-03 14:35:46 -0500

    add contrib distribution, consolidated into one setup.py file

---
+++ software/setup.py
@@ -0,0 +1,30 @@
+from email.policy import default
+import sys
+
+from setuptools import setup
+
+DEFAULTS = dict(
+    version="0.0.2.dev2",
+    url="https://github.com/Allen-Synthesis/EuroPi",
+    author="Allen Synthesis",
+    author_email="contact@allensynthesis.co.uk",
+    license="Apache 2.0",
+)
+
+setup(
+    name="micropython-europi",
+    description="EuroPi module for MicroPython",
+    long_description="EuroPi module for MicroPython",
+    package_dir={'': "firmware"},
+    packages=[""],
+    **DEFAULTS,
+)
+
+setup(
+    name="micropython-europi-contrib",
+    description="Community Contributions to EuroPi module for MicroPython",
+    long_description="Community Contributions to EuroPi module for MicroPython",
+    packages=["contrib"],
+    namespace_packages=["contrib"],
+    **DEFAULTS,
+)
