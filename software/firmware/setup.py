c7cd0c4f30311fd8bf53b8ff7c31055ef740d9d5
Matthew Jaskula <mjaskula@users.noreply.github.com>
2022-03-09 14:53:06 -0500

    Update long description
    based on @awonak suggestion
    
    Co-authored-by: Adam Wonak <adam.wonak@gmail.com>

--- software/firmware/setup.py
+++ software/firmware/setup.py
@@ -6,7 +6,7 @@ setup(
     name="micropython-europi",
     version="0.0.2.dev4",
     description="EuroPi module for MicroPython",
-    long_description="",
+    long_description="""The EuroPi is a fully user reprogrammable EuroRack module based on the Raspberry Pi Pico, which allows users to process inputs and controls to produce outputs based on code written in Python.""",
     url="https://github.com/Allen-Synthesis/EuroPi",
     author="Allen Synthesis",
     author_email="contact@allensynthesis.co.uk",
