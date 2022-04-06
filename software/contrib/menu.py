27f64119fa23f5a01ba6595ab2a31fb71e39222d
Sean Bechhofer <sean.bechhofer@manchester.ac.uk>
2022-04-06 21:46:22 +0100

    Adding Hamlet

--- software/contrib/menu.py
+++ software/contrib/menu.py
@@ -4,13 +4,14 @@ from bootloader import BootloaderMenu
 from contrib.coin_toss import CoinToss
 from contrib.consequencer import Consequencer
 from contrib.diagnostic import Diagnostic
+from contrib.hamlet import Hamlet
 from contrib.harmonic_lfos import HarmonicLFOs
 from contrib.hello_world import HelloWorld
 from contrib.noddy_holder import NoddyHolder
 from contrib.polyrhythmic_sequencer import PolyrhythmSeq
 from contrib.radio_scanner import RadioScanner
 from contrib.scope import Scope
-from contrib.strange_attractor StrangeAttractor
+from contrib.strange_attractor import StrangeAttractor
 from calibrate import Calibrate
 
 # Scripts that are included in the menu
@@ -18,6 +19,7 @@ EUROPI_SCRIPT_CLASSES = [
     CoinToss,
     Consequencer,
     Diagnostic,
+    Hamlet,
     HarmonicLFOs,
     HelloWorld,
     NoddyHolder,
