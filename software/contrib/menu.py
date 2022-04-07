d9163573aa6ddfee122cde613e59a1875dc0a3ed
Matthew Jaskula <mjaskula@gmail.com>
2022-04-07 09:45:26 -0400

    fix StrangeAttractor import

--- software/contrib/menu.py
+++ software/contrib/menu.py
@@ -10,7 +10,7 @@ from contrib.noddy_holder import NoddyHolder
 from contrib.polyrhythmic_sequencer import PolyrhythmSeq
 from contrib.radio_scanner import RadioScanner
 from contrib.scope import Scope
-from contrib.strange_attractor StrangeAttractor
+from contrib.strange_attractor import StrangeAttractor
 from calibrate import Calibrate
 
 # Scripts that are included in the menu
