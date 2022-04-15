26e6490afff7376dd07affa1a99ddb529b39d1fb
Rory Allen <79809962+roryjamesallen@users.noreply.github.com>
2022-04-15 22:16:43 +0100

    Add comments

--- software/contrib/harmonic_lfos.py
+++ software/contrib/harmonic_lfos.py
@@ -5,20 +5,23 @@ from time import sleep_ms
 from machine import freq
 from europi_script import EuroPiScript
 
+#Define maximum voltage and maximum harmonic (the harmonic value may become hard to choose using the knob if you use too high of a maximum)
 MAX_VOLTAGE = 10
 MAX_HARMONIC = 32
 
+#Overclock the Pico for improved performance
 freq(250_000_000)
 
 class HarmonicLFOs(EuroPiScript):
-
     def __init__(self):
         super().__init__()
         state = self.load_state_json()
         
+        #Use the saved values for the LFO divisions and mode if found in the save state file, using defaults if not
         self.HARMONICS = state.get("HARMONICS", [1, 3, 5, 7, 11, 13])
         self.MODE = state.get("MODE", 0)
         
+        #Initialise all the other variables
         self.degree = 0
         self.delay, self.increment_value = self.get_delay_increment_value()
         self.pixel_x = OLED_WIDTH-1
@@ -26,16 +29,20 @@ class HarmonicLFOs(EuroPiScript):
         self.selected_lfo = 0
         self.selected_lfo_start_value = self.read_k2()
         
+        #Set the digital input and button handlers
         din.handler(self.reset)
         b1.handler(self.change_mode)
         b2.handler(self.increment_selection)
 
+    #Read knob 2 as a new division between 1 and MAX_HARMONIC
     def read_k2(self):
         return k2.read_position(MAX_HARMONIC) + 1
 
+    #Reset all LFOs to zero, maintaining their divisions
     def reset(self):
         self.degree = 0
 
+    #Change between sine, saw, and square wave LFOs
     def change_mode(self):
         self.MODE += 1
         
@@ -44,18 +51,20 @@ class HarmonicLFOs(EuroPiScript):
             
         self.save_state()
 
+    #Determine the time to wait between steps (this controls the master rate of the LFOs)
     def get_delay_increment_value(self):
         delay = (0.1 - (k1.read_position(100, 1)/1000)) + (ain.read_voltage(1)/100)
         return delay, round((((1/delay)-10)/1)+1)
 
+    #Move on to select the next LFO to change the division
     def increment_selection(self):
         self.selected_lfo += 1
         if self.selected_lfo == 6:
             self.selected_lfo = 0
         self.selected_lfo_start_value = self.read_k2()
 
+    #Save the current values of HARMONICS and MODE to the save state file
     def save_state(self):
-        """Save the current state variables as JSON."""
         # Don't save if it has been less than 5 seconds since last save.
         if self.last_saved() < 5000:
             return
@@ -67,8 +76,6 @@ class HarmonicLFOs(EuroPiScript):
         self.save_state_json(state)
 
     def main(self):
-        global degree, delay, increment_value, selected_lfo, selected_lfo_start_value
-
         while True:
             rad = radians(self.degree)
             
@@ -79,6 +86,7 @@ class HarmonicLFOs(EuroPiScript):
                 self.HARMONICS[self.selected_lfo] = self.k2_reading
                 self.save_state()
             
+            #Draw the lines for the LFOs on the display
             oled.vline(self.pixel_x,0,OLED_HEIGHT,0)
             for cv, multiplier in zip(cvs, self.HARMONICS):
                 three_sixty = 360*multiplier
