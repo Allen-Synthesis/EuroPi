4b1b5fa09e73c02e446d29da30ec699da68af56a
Matthew Jaskula <mjaskula@gmail.com>
2022-05-05 12:13:59 -0400

    add support for DigitalReader to MockHardware
    also add a test for DigitalReader

--- software/tests/mock_hardware.py
+++ software/tests/mock_hardware.py
@@ -1,4 +1,6 @@
-from machine import ADC
+from machine import ADC, Pin
+
+from europi import AnalogueReader, DigitalReader
 
 class MockHardware:
     """A class used in tests to stand in for actual EuroPi hardware. Allows a test to set the values for various
@@ -8,12 +10,19 @@ class MockHardware:
     def __init__(self, monkeypatch):
         self._monkeypatch = monkeypatch
         self._adc_pin_values = {}
+        self._digital_pin_values = {}
 
         self._patch()
 
     def _patch(self):
         self._monkeypatch.setattr(ADC, "read_u16", lambda pin: self._adc_pin_values[pin])
+        self._monkeypatch.setattr(Pin, "value", lambda pin: self._digital_pin_values[pin])
 
-    def set_ADC_u16_value(self, component, value):
-        """Sets the value that will be returned by a call to `read_u16` on the given component."""
-        self._adc_pin_values[component.pin] =  value
+    def set_ADC_u16_value(self, reader: AnalogueReader, value: int):
+        """Sets the value that will be returned by a call to `read_u16` on the given AnalogueReader."""
+        self._adc_pin_values[reader.pin] =  value
+
+    def set_digital_value(self, reader: DigitalReader, value: bool):
+        """Sets the value that will be returned by a call to `value` on the given DigitalReader."""
+        self._digital_pin_values[reader.pin] =  not value
+
