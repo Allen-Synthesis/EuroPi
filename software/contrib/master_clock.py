9dd75e04baf96dd74c4bece2c7bb05512145167b
Nik <anselln@amazon.com>
2022-07-30 14:49:42 +0100

    PW now set as a percentage within min and max limits

--- software/contrib/master_clock.py
+++ software/contrib/master_clock.py
@@ -30,8 +30,6 @@ output_4: clock / 8
 output_5: clock / 16
 output_6: clock / 32
 
-Known bugs: Changing the pulse width affects tempo. This could be some modules using the falling edge to drive their clock, or some miscalcuation in the code... more investigation required!
-
 '''
 
 class MasterClock(EuroPiScript):
@@ -46,13 +44,12 @@ class MasterClock(EuroPiScript):
         self.CLOCKS_PER_QUARTER_NOTE = 4
         self.MIN_BPM = 50
         self.MAX_BPM = 200
-        self.MIN_PULSE_WIDTH = 7
-        self.MAX_PULSE_WIDTH = 50
+        self.MIN_PULSE_WIDTH = 8
         self.resetTimeout = 2000
-        self.lastStep = 0
-        self.delta = 0
+        self.previousStepTime = 0
 
-        self.timeToSleepMs = self.getSleepTime()
+        self.getSleepTime()
+        self.MAX_PULSE_WIDTH = self.timeToSleepMs // 2
 
         # Get asyncio event loop object
         self.el = asyncio.get_event_loop()
@@ -63,11 +60,12 @@ class MasterClock(EuroPiScript):
         def StartStop():
             self.running = not self.running
 
-        # Reset step count
+        # Reset step count with button press
         @b2.handler_falling
         def resetButton():
             self.step = 1
         
+        # Reset step count upon receiving a din voltage
         @din.handler
         def resetDin():
             self.step = 1
@@ -78,19 +76,22 @@ class MasterClock(EuroPiScript):
         await asyncio.sleep_ms(self.pulseWidthMs)
         cv.off()
 
-    # Calculdates the time to sleep between clock pulses
+    # Given a desired BPM, calculate the time to sleep between clock pulses
     def getSleepTime(self):
-        return int((60000 / self.bpm / self.CLOCKS_PER_QUARTER_NOTE) - self.pulseWidthMs)
+        self.timeToSleepMs = int((60000 / self.bpm / self.CLOCKS_PER_QUARTER_NOTE))
     
     def getBPM(self):
-        return self.MIN_BPM + k1.read_position(steps=(self.MAX_BPM - self.MIN_BPM + 1), samples=512)
+        self.bpm = self.MIN_BPM + k1.read_position(steps=(self.MAX_BPM - self.MIN_BPM + 1), samples=512)
 
     def getPulseWidth(self):
-        pw = self.MIN_PULSE_WIDTH + k2.read_position(steps=(self.MAX_PULSE_WIDTH - self.MIN_PULSE_WIDTH + 1), samples=512)
-        if pw > self.timeToSleepMs / 2:
-            return int(self.timeToSleepMs / 2)
-        else:
-            return pw
+        # Get desired PW percent
+        self.pulseWidthPercent = k2.read_position(steps=50, samples=512) + 1
+        # Calc Pulse Width duration (x 2 needed because the max is 50%)
+        self.pulseWidthMs = int((self.MAX_PULSE_WIDTH * 2) * (self.pulseWidthPercent / 100)) 
+        # Don't allow a pulse width less than the minimum
+        if self.pulseWidthMs < self.MIN_PULSE_WIDTH:
+            self.pulseWidthMs = self.MIN_PULSE_WIDTH
+            self.pulseWidthPercent = 'min'
 
     # Triggered by main. Sends output pulses at required division
     def clockTrigger(self):
@@ -118,37 +119,34 @@ class MasterClock(EuroPiScript):
             self.step = 1
         
         # Get time of last step to use in the auto reset function
-        self.lastStep = time.ticks_ms()
+        self.previousStepTime = time.ticks_ms()
 
     async def main(self):
         while True:
-            self.bpm = self.getBPM()
-            self.pulseWidthMs = self.getPulseWidth()
+            self.getBPM()
+            self.getSleepTime()
+            self.MAX_PULSE_WIDTH = self.timeToSleepMs // 2  # Maximum of 50% pulse width
+            self.getPulseWidth()
             self.updateScreen()
 
             # Auto reset function after resetTimeout
-            if self.step != 0 and ticks_diff(ticks_ms(), self.lastStep) > self.resetTimeout:
+            if self.step != 0 and ticks_diff(ticks_ms(), self.previousStepTime) > self.resetTimeout:
                  self.step = 1
 
             if self.running:
-                # get time before clock triggers
-                t = time.ticks_us()
                 self.clockTrigger()
-                self.timeToSleepMs = self.getSleepTime()
-                # calcuate time to sleep based on elasped time
-                self.delta = time.ticks_diff(time.ticks_us(), t)
-                print(self.delta)
                 await asyncio.sleep_ms(self.timeToSleepMs)
 
     def updateScreen(self):
         # oled.clear() - dont use this, it causes the screen to flicker!
         oled.fill(0)
         oled.text('BPM: ' + str(self.bpm), 0, 1, 1)
-        oled.text('PW: ' + str(self.pulseWidthMs) + 'ms', 0, 12, 1)
-        #oled.text(str(self.delta), 90, 12, 1)
+        oled.text('PW: ' + str(self.pulseWidthPercent) + '% (' + str(self.pulseWidthMs) + 'ms)', 0, 12, 1)
         oled.text(str(self.step), 112, 0, 1)
         if not self.running:
-            oled.text('B1:Start...', 0, 23, 1)
+            oled.text('B1:Start', 0, 23, 1)
+        else:
+            oled.text('B1:Stop', 0, 23, 1)
         oled.show()
 
 if __name__ == '__main__':
