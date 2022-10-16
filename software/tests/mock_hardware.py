09f3247602f6eb4f6e83a9b519f8c55e0ce6e3c2
Nik <anselln@amazon.com>
2022-10-16 11:28:27 +0100

    upstream sync

--- software/tests/mock_hardware.py
+++ software/tests/mock_hardware.py
@@ -26,3 +26,12 @@ class MockHardware:
     def set_digital_value(self, reader: DigitalReader, value: bool):
         """Sets the value that will be returned by a call to `value` on the given DigitalReader."""
         self._digital_pin_values[reader.pin] = not value
+
+    def set_percent(self, reader: AnalogueReader, value: float):
+        self.set_ADC_u16_value(reader, value * MAX_UINT16)
+
+    def set_analogue_input_percent(self, reader: AnalogueReader, value: float):
+        self.set_ADC_u16_value(reader, value * INPUT_CALIBRATION_VALUES[-1])
+
+    def set_knob_percent(self, knob: Knob, value: float):
+        self.set_ADC_u16_value(knob, (1 - value) * MAX_UINT16)
