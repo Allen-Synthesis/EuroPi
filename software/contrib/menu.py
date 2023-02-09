8bc77e21e017a6c776a1107937e855e389542b4e
Matthew Jaskula <mjaskula@gmail.com>
2023-02-09 11:30:36 -0500

    display a progress bar while loading scripts for the menu

--- software/contrib/menu.py
+++ software/contrib/menu.py
@@ -1,6 +1,6 @@
 """See menu.md for details."""
 # Reset the module state and display bootsplash screen.
-from europi import bootsplash, usb_connected
+from europi import OLED_HEIGHT, OLED_WIDTH, bootsplash, usb_connected, oled
 
 #  This is a fix for a USB connection issue documented in GitHub issue #179, and its removal condition is set out in GitHub issue #184
 if usb_connected.value() == 0:
@@ -10,61 +10,52 @@ if usb_connected.value() == 0:
 
 bootsplash()
 
+
+def progress_bar(percentage):
+    oled.hline(0, OLED_HEIGHT - 1, int(OLED_WIDTH * percentage), 1)
+    oled.show()
+
+
 from bootloader import BootloaderMenu
 
-# from contrib.knob_playground import KnobPlayground
-from contrib.bernoulli_gates import BernoulliGates
-from contrib.coin_toss import CoinToss
-from contrib.consequencer import Consequencer
-from contrib.cvecorder import CVecorder
-from contrib.diagnostic import Diagnostic
-from contrib.hamlet import Hamlet
-from contrib.harmonic_lfos import HarmonicLFOs
-from contrib.hello_world import HelloWorld
-from contrib.logic import Logic
-from contrib.master_clock import MasterClock
-from contrib.noddy_holder import NoddyHolder
-from contrib.piconacci import Piconacci
-from contrib.polyrhythmic_sequencer import PolyrhythmSeq
-from contrib.poly_square import PolySquare
-from contrib.probapoly import Probapoly
-from contrib.quantizer import QuantizerScript
-from contrib.radio_scanner import RadioScanner
-from contrib.scope import Scope
-from contrib.sequential_switch import SequentialSwitch
-from contrib.smooth_random_voltages import SmoothRandomVoltages
-from contrib.strange_attractor import StrangeAttractor
-from contrib.turing_machine import EuroPiTuringMachine
-from calibrate import Calibrate
-
 # Scripts that are included in the menu
-EUROPI_SCRIPT_CLASSES = [
-    # KnobPlayground,
-    BernoulliGates,
-    CoinToss,
-    Consequencer,
-    CVecorder,
-    Diagnostic,
-    Hamlet,
-    HarmonicLFOs,
-    HelloWorld,
-    Logic,
-    MasterClock,
-    NoddyHolder,
-    Piconacci,
-    PolyrhythmSeq,
-    PolySquare,
-    Probapoly,
-    QuantizerScript,
-    RadioScanner,
-    Scope,
-    SequentialSwitch,
-    SmoothRandomVoltages,
-    StrangeAttractor,
-    EuroPiTuringMachine,
-    Calibrate,
+scripts = [
+    ("contrib.knob_playground", "KnobPlayground"),
+    ("contrib.bernoulli_gates", "BernoulliGates"),
+    ("contrib.coin_toss", "CoinToss"),
+    ("contrib.consequencer", "Consequencer"),
+    ("contrib.cvecorder", "CVecorder"),
+    ("contrib.diagnostic", "Diagnostic"),
+    ("contrib.hamlet", "Hamlet"),
+    ("contrib.harmonic_lfos", "HarmonicLFOs"),
+    ("contrib.hello_world", "HelloWorld"),
+    ("contrib.logic", "Logic"),
+    ("contrib.master_clock", "MasterClock"),
+    ("contrib.noddy_holder", "NoddyHolder"),
+    ("contrib.piconacci", "Piconacci"),
+    ("contrib.polyrhythmic_sequencer", "PolyrhythmSeq"),
+    ("contrib.poly_square", "PolySquare"),
+    ("contrib.probapoly", "Probapoly"),
+    ("contrib.quantizer", "QuantizerScript"),
+    ("contrib.radio_scanner", "RadioScanner"),
+    ("contrib.scope", "Scope"),
+    ("contrib.sequential_switch", "SequentialSwitch"),
+    ("contrib.smooth_random_voltages", "SmoothRandomVoltages"),
+    ("contrib.strange_attractor", "StrangeAttractor"),
+    ("contrib.turing_machine", "EuroPiTuringMachine"),
+    ("calibrate", "Calibrate"),
 ]
 
+
+def generate_script_classes(scripts) -> "List[str]":
+    c = []
+    for i, (module, clazz) in enumerate(scripts):
+        c.append(getattr(__import__(module, None, None, [None]), clazz))
+        progress_bar(i / len(scripts))
+    return c
+
+
+EUROPI_SCRIPT_CLASSES = generate_script_classes(scripts)
 
 if __name__ == "__main__":
     BootloaderMenu(EUROPI_SCRIPT_CLASSES).main()
