e1a53d4cecd0fae9e1974a1886ccef075c2fe173
François Georgy <francois.georgy@gmail.com>
2023-02-16 07:58:31 +0100

    Source reformatted with black.

--- software/contrib/largefont_demo.py
+++ software/contrib/largefont_demo.py
@@ -18,7 +18,6 @@ import freesans24
 
 
 class LargeFontDemo(EuroPiScript):
-
     @classmethod
     def display_name(cls):
         return "Large font demo"
@@ -26,21 +25,21 @@ class LargeFontDemo(EuroPiScript):
     def __init__(self):
         super().__init__()
         self.demo = 4
-        self.boxed = True   # alternate one round boxed, one round unboxed.
+        self.boxed = True  # alternate one round boxed, one round unboxed.
 
     def update_demo(self):
         self.demo = (self.demo + 1) % 5
         if self.demo == 0:
-            oled.centre_text('Default\nmonospaced\n8x8 font')
+            oled.centre_text("Default\nmonospaced\n8x8 font")
             self.boxed = not self.boxed
         elif self.demo == 1:
-            oled.centre_text('14 ABC gpq\n0123456 gp', font=freesans14)
+            oled.centre_text("14 ABC gpq\n0123456 gp", font=freesans14)
         elif self.demo == 2:
-            oled.centre_text('17 ABC gp', font=freesans17)
+            oled.centre_text("17 ABC gp", font=freesans17)
         elif self.demo == 3:
-            oled.centre_text('20 ABC gp', font=freesans20)
+            oled.centre_text("20 ABC gp", font=freesans20)
         elif self.demo == 4:
-            oled.centre_text('24 ABC gp', font=freesans24)
+            oled.centre_text("24 ABC gp", font=freesans24)
         if self.boxed:
             oled.rect(0, 0, OLED_WIDTH, OLED_HEIGHT, 1)
             oled.show()
