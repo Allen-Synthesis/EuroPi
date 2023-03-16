e5e285dd1a53b5c650576dcdca2d3857516bcbd1
redoxcode <117983047+redoxcode@users.noreply.github.com>
2023-03-16 14:34:47 +0100

    Why did my black output differ?

--- software/firmware/europi.py
+++ software/firmware/europi.py
@@ -195,18 +195,14 @@ class AnalogueInput(AnalogueReader):
     wan't incredibly accurate (but quite slow) readings.
     """
 
-    def __init__(
-        self, pin, min_voltage=MIN_INPUT_VOLTAGE, max_voltage=MAX_INPUT_VOLTAGE
-    ):
+    def __init__(self, pin, min_voltage=MIN_INPUT_VOLTAGE, max_voltage=MAX_INPUT_VOLTAGE):
         super().__init__(pin)
         self.MIN_VOLTAGE = min_voltage
         self.MAX_VOLTAGE = max_voltage
         self._gradients = []
         for index, value in enumerate(INPUT_CALIBRATION_VALUES[:-1]):
             try:
-                self._gradients.append(
-                    1 / (INPUT_CALIBRATION_VALUES[index + 1] - value)
-                )
+                self._gradients.append(1 / (INPUT_CALIBRATION_VALUES[index + 1] - value))
             except ZeroDivisionError:
                 raise Exception(
                     "The input calibration process did not complete properly. Please complete again with rack power turned on"
@@ -239,9 +235,7 @@ class AnalogueInput(AnalogueReader):
             )
         else:
             index = int(percent * (len(INPUT_CALIBRATION_VALUES) - 1))
-            cv = index + (
-                self._gradients[index] * (reading - INPUT_CALIBRATION_VALUES[index])
-            )
+            cv = index + (self._gradients[index] * (reading - INPUT_CALIBRATION_VALUES[index]))
         return clamp(cv, self.MIN_VOLTAGE, self.MAX_VOLTAGE)
 
 
@@ -324,18 +318,12 @@ class DigitalReader:
     def _bounce_wrapper(self, pin):
         """IRQ handler wrapper for falling and rising edge callback functions."""
         if self.value() == HIGH:
-            if (
-                time.ticks_diff(time.ticks_ms(), self.last_rising_ms)
-                < self.debounce_delay
-            ):
+            if time.ticks_diff(time.ticks_ms(), self.last_rising_ms) < self.debounce_delay:
                 return
             self.last_rising_ms = time.ticks_ms()
             return self._rising_handler()
         else:
-            if (
-                time.ticks_diff(time.ticks_ms(), self.last_falling_ms)
-                < self.debounce_delay
-            ):
+            if time.ticks_diff(time.ticks_ms(), self.last_falling_ms) < self.debounce_delay:
                 return
             self.last_falling_ms = time.ticks_ms()
 
@@ -523,9 +511,7 @@ class Output:
     calibration is important if you want to be able to output precise voltages.
     """
 
-    def __init__(
-        self, pin, min_voltage=MIN_OUTPUT_VOLTAGE, max_voltage=MAX_OUTPUT_VOLTAGE
-    ):
+    def __init__(self, pin, min_voltage=MIN_OUTPUT_VOLTAGE, max_voltage=MAX_OUTPUT_VOLTAGE):
         self.pin = PWM(Pin(pin))
         self.pin.freq(PWM_FREQ)
         self._duty = 0
@@ -548,9 +534,7 @@ class Output:
             return self._duty / MAX_UINT16
         voltage = clamp(voltage, self.MIN_VOLTAGE, self.MAX_VOLTAGE)
         index = int(voltage // 1)
-        self._set_duty(
-            OUTPUT_CALIBRATION_VALUES[index] + (self._gradients[index] * (voltage % 1))
-        )
+        self._set_duty(OUTPUT_CALIBRATION_VALUES[index] + (self._gradients[index] * (voltage % 1)))
 
     def on(self):
         """Set the voltage HIGH at 5 volts."""
