dd76f5a656ae6bc2b43a09ea634aa63a7260947b
redoxcode <117983047+redoxcode@users.noreply.github.com>
2023-03-16 14:04:53 +0100

    Fixed error in percentage calculation

--- software/firmware/europi.py
+++ software/firmware/europi.py
@@ -214,12 +214,12 @@ class AnalogueInput(AnalogueReader):
         # Determine the percent value from the max calibration value.
         reading = self._sample_adc(samples)
         max_value = max(reading - INPUT_CALIBRATION_VALUES[0], INPUT_CALIBRATION_VALUES[-1] - INPUT_CALIBRATION_VALUES[0])
-        return reading / max_value
+        return reading - INPUT_CALIBRATION_VALUES[0] / max_value
 
     def read_voltage(self, samples=None):
         reading = self._sample_adc(samples)
         max_value = max(reading - INPUT_CALIBRATION_VALUES[0], INPUT_CALIBRATION_VALUES[-1] - INPUT_CALIBRATION_VALUES[0])
-        percent = reading / max_value
+        percent = reading - INPUT_CALIBRATION_VALUES[0] / max_value
         # low precision vs. high precision
         if len(self._gradients) == 2:
             cv = 10 * (reading  - INPUT_CALIBRATION_VALUES[0] / INPUT_CALIBRATION_VALUES[-1] - INPUT_CALIBRATION_VALUES[0])
