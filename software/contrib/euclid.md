b21b32c549670dbef6f28d7abc715cd381c18b1a
Chris I-B <c.iverachbrereton@gmail.com>
2023-03-19 14:50:53 -0400

    Implement the bulk of the backend for generating the rhythms, loading, and saving. Add the euclid docs and add the script to the menu

---
+++ software/contrib/euclid.md
@@ -0,0 +1,9 @@
+# Euclid
+
+This script generates six different Euclidean rhythms, one
+on each output. The length, number of steps, and rotation
+of each pattern can be independently adjusted.
+
+## I/O Assignments
+
+- `din`: external clock input
--- software/contrib/euclid.py
+++ software/contrib/euclid.py
@@ -3,8 +3,13 @@
 
 @author Chris Iverach-Brereton <ve4cib@gmail.com>
 @author Brian House
-@year   2023
+@year   2011, 2023
+
+@license MIT
 """
+
+from europi import *
+from europi_script import EuroPiScript
 
 def generate_euclidean_pattern(steps, pulses, rot=0):
     """Generates an array indicating the on/off steps of Euclid(k, n)
@@ -18,6 +23,8 @@ def generate_euclidean_pattern(steps, pulses, rot=0):
     @return An int array of length steps consisting of 1 and 0 values only
     
     @exception ValueError if pulses or rot is out of range
+    
+    @license MIT -- https://github.com/brianhouse/bjorklund/blob/master/LICENSE.txt
     """
     steps = int(steps)
     pulses = int(pulses)
@@ -65,5 +72,151 @@ def generate_euclidean_pattern(steps, pulses, rot=0):
     
     return pattern
 
-class EuclideanRhythms(EuroPiScript):
+class EuclidGenerator:
+    """Generates the euclidean rhythm for a single output
+    """
     
+    def __init__(self, cv_pin):
+        """Create a generator that sends its output to the given pin
+
+        @param cv_pin  One of the six output jacks (cv1..cv6)
+        """
+        
+        ## The CV output this generator controls
+        self.cv = output_pin
+        
+        ## The current position within the pattern
+        self.position = 0
+        
+        ## The number of steps in the pattern
+        self.steps = 0
+        
+        ## The number of triggers in the pattern
+        self.pulses = 0
+        
+        ## The rotation of the pattern
+        self.rotation = 0
+        
+        ## The on/off pattern we generate
+        self.pattern = []
+        
+    def regenerate(self):
+        """Re-calculate the pattern for this generator
+
+        Call this after changing any of steps, pulses, or rotation to apply
+        the changes.
+        
+        Changing the pattern will reset the position to zero
+        """
+        
+        self.pattern = generate_euclidean_pattern(self.steps, self.pulses, self.rotation)
+        self.position = 0
+        
+    def advance(self):
+        """Advance to the next step in the pattern and set the CV output
+        """
+        
+        # advance the position
+        # to ease CPU usage don't do any divisions, just reset to zero
+        # if we overflow
+        self.position = self.position+1
+        if self.position >= len(self.pattern):
+            self.position = 0
+            
+        if self.pattern[self.position] == 0:
+            self.cv.off()
+        else:
+            self.cv.on()
+        
+
+class EuclideanRhythms(EuroPiScript):
+    """Generates 6 different Euclidean rhythms, one per output
+
+    Must be clocked externally into DIN
+    """
+    
+    def __init__(self):
+        super().__init__()
+        
+        self.generators = [
+            EuclidGenerator(cv1),
+            EuclidGenerator(cv2),
+            EuclidGenerator(cv3),
+            EuclidGenerator(cv4),
+            EuclidGenerator(cv5),
+            EuclidGenerator(cv6)
+        ]
+        
+        self.load()
+        
+        @din.handler
+        def on_rising_clock():
+            """Handler for the rising edge of the input clock
+
+            Advance all of the rhythms
+            """
+            for g in self.generators:
+                g.advance()
+                
+        @din.handler_falling
+        def on_falling_clock():
+            """Handler for the falling edge of the input clock
+
+            Turn off all of the CVs so we don't stay on for adjacent pulses
+            """
+            for cv in cvs:
+                cv.off()
+            
+        @b1.handler
+        def on_b1_press():
+            """Handler for pressing button 1
+            """
+            pass
+            
+        @b2.handler
+        def on_b2_press():
+            """Handler for pressing button 2
+            """
+            pass
+        
+    @classmethod
+    def display_name(cls):
+        return "Euclid"
+        
+    def load(self):
+        """Load the previously-saved parameters and restore them
+        """
+        state = self.load_state_json()
+        
+        for rhythm in state.get("rhythms", []):
+            id = rhythm["id"]
+            generator = self.generators[id]
+            
+            generator.steps = rhythm["steps"]
+            generator.pulses = rhythm["pulses"]
+            generator.rotation = rhythm["rotation"]
+            
+            generator.regenerate()
+        
+    def save(self):
+        """Write the current settings to the persistent storage
+        """
+        state = []
+        for i in range(len(self.generators)):
+            d = {
+                "id": i,
+                "steps": self.generators[i].steps,
+                "pulses": self.generators[i].pulses,
+                "rotation": self.generators[i].rotation,
+            }
+            state.append(d)
+        self.save_state_json(state)
+        
+    def main(self):
+        while True:
+            # TODO: implement the UI logic
+            # for now, just sleep forever
+            time.sleep(0.01)
+    
+if __name__=="__main__":
+    EuclideanRhythms().main()
