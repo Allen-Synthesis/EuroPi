ee423aeea4405fa320e549657646b3f42f853c79
Chris I-B <c.iverachbrereton@gmail.com>
2023-03-24 22:55:26 -0400

    Add the full MIT license to the relevant function. Note in the top of the script that there is a MIT-licensed component, but don't license the _whole_ script under MIT

--- software/contrib/euclid.py
+++ software/contrib/euclid.py
@@ -5,7 +5,8 @@
 @author Brian House
 @year   2011, 2023
 
-@license MIT
+This script contains code released under the MIT license, as
+noted below
 """
 
 from europi import *
@@ -30,7 +31,27 @@ def generate_euclidean_pattern(steps, pulses, rot=0):
     
     @exception ValueError if pulses or rot is out of range
     
-    @license MIT -- https://github.com/brianhouse/bjorklund/blob/master/LICENSE.txt
+    @author Brian House
+    @year 2011
+    @license MIT
+    
+    COPYRIGHT 2011 Brian House
+    
+    Permission is hereby granted, free of charge, to any person obtaining a copy of this
+    software and associated documentation files (the “Software”), to deal in the Software
+    without restriction, including without limitation the rights to use, copy, modify, merge,
+    publish, distribute, sublicense, and/or sell copies of the Software, and to permit
+    persons to whom the Software is furnished to do so, subject to the following conditions:
+
+    The above copyright notice and this permission notice shall be included in all copies or
+    substantial portions of the Software.
+
+    THE SOFTWARE IS PROVIDED “AS IS”, WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED,
+    INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR
+    PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE
+    FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR
+    OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+    DEALINGS IN THE SOFTWARE.
     """
     steps = int(steps)
     pulses = int(pulses)
