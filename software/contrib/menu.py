d512b101f91d02122737e7d86a04fdcbcfd0789c
Nik Ansell <gamecat69@hotmail.com>
2023-05-01 11:17:27 +0100

    add to menu

--- software/contrib/menu.py
+++ software/contrib/menu.py
@@ -20,6 +20,7 @@ EUROPI_SCRIPTS = [
     "contrib.consequencer.Consequencer",
     "contrib.cvecorder.CVecorder",
     "contrib.diagnostic.Diagnostic",
+    "contrib.egressus_melodiam.EgressusMelodium",
     "contrib.envelope_generator.EnvelopeGenerator",
     "contrib.euclid.EuclideanRhythms",
     "contrib.hamlet.Hamlet",
