8c87c6ce204976d5105f74d3fbf95104a617a850
Chris I-B <ve4cib@gmail.com>
2023-10-28 17:49:21 -0400

    Overhaul the script to use a single digital input, allow CV control over the gate output, add a toggle output, and two new end-of-fall outputs (for the dynamic gate & toggle outs)

--- software/contrib/gates_and_triggers.md
+++ software/contrib/gates_and_triggers.md
@@ -1,25 +1,31 @@
 # Gates and Triggers (G&T)
 
 This program is inspired by the [After Later Audio G&T](https://afterlateraudio.com/products/gt-gates-and-triggers)
-module.  The program treats `din` and `ain` both as digital inputs and outputs trigger/gate signals on `cv1` and `cv4`.
+module.  The program converts a gate or trigger signal on `din` into a gate signal with a variable length and
+rising/falling edge triggers.
 
 ## I/O Mapping
 
 | I/O           | Usage
 |---------------|-------------------------------------------------------------------|
-| `din`         | Incoming signal for `cv1`, `cv2`, and `cv3`                       |
-| `ain`         | Incoming signal for `cv4`, `cv5`, and `cv5`                       |
+| `din`         | Incoming gate or trigger signal                                   |
+| `ain`         | CV control over output gate length                                |
 | `b1`          | Manual input equivalent to `din`                                  |
-| `b2`          | Manual input equivalent to `ain`                                  |
-| `k1`          | Gate duration control for `cv1`                                   |
-| `k2`          | Gate duration control for `cv2`                                   |
+| `b2`          | Manual toggle input (see below)                                   |
+| `k1`          | Primary gate duration control                                     |
+| `k2`          | Attenuator for CV input on `ain`                                  |
 | `cv1`         | Gate output for `din`/`b1`                                        |
 | `cv2`         | Trigger output for the rising edge of `din`/`b1`                  |
 | `cv3`         | Trigger output for the falling edge of `din`/`b1`                 |
-| `cv4`         | Gate output for `ain`/`b2`                                        |
-| `cv5`         | Trigger output for the rising edge of `ain`/`b2`                  |
-| `cv6`         | Trigger output for the falling edge of `ain`/`b2`                 |
+| `cv4`         | Trigger output for the falling edge of `cv1`                      |
+| `cv5`         | Toggle output; changes state on every incoming rising edge        |
+| `cv6`         | Trigger output for the falling edge of `cv5`                      |
 
-The gate duration goes from 10ms to 2s, depending on the position of `k1`/`k2`.  The knob response is non-linear, so
-there's more precision at the longer-end of the range.  Trigger outputs are always 10ms long.  All output signals are
-5V.
+The gate output on `cv1` has a normal range of 10ms to 2s, depending on the position of `k1`.  CV input to `ain` can
+increase this duration.
+
+`ain` expects an input range of 0-10V.  `k2` acts as an attenuator for this input signal.
+
+`cv5` and `cv6` change state every time an incoming gate/trigger starts on `din` or when `b1` or `b2` is pressed.
+Pressing `b2` will ONLY change the state of `cv5` and `cv6`, while `b1` will also output gate and trigger signals on
+`cv1`-`cv4`.
--- software/contrib/gates_and_triggers.py
+++ software/contrib/gates_and_triggers.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python3
 """Convert incoming triggers to gates or gates to triggers
 
-Buttons allow manually creating gates/triggers, knobs control the duration of the output signals.  Both AIN and DIN
-are used as digital inputs for incoming gates/triggers.
+Also outputs a toggle input which changes state every time an incoming gate is received, or when either button is
+pressed.
 
 @author Chris Iverach-Brereton <ve4cib@gmail.com>
 @year   2023
@@ -20,116 +20,102 @@ OUTPUT_VOLTAGE = 5.0
 ## Trigger outputs are 10ms long (rising/falling edges of gate signals)
 TRIGGER_DURATION_MS = 10
 
-class A2D_Wrapper:
-    """Wraps an AnalogReader to allow it to simulate a DigitalReader
-    """
-    def __init__(self, ain, debounce=1, high_low_cutoff=0.8, \
-                 cb_rising=lambda: None, cb_falling=lambda: None):
-        """Constructor
+class GatesAndTriggers(EuroPiScript):
+    def __init__(self):
+        super().__init__()
 
-        @param ain  The AnalogReader we're wrapping
-        @param debounce  The number of consecutive high/low signals needed to flip the digital state
-        @param high_low_cutoff  The threshold at which the analog signal is considered high
+        self.on_incoming_rise_start_time = 0
+        self.on_incoming_fall_start_time = 0
+        self.on_gate_fall_start_time = 0
+        self.on_toggle_fall_start_time = 0
 
-        @param cb_rising  A function to call on the rising edge of the signal
-        @param cb_falling  A function to call on the falling edge of the signal
-        """
-        self.ain = ain
-        self.debounce = debounce
-        self.high_low_cutoff = high_low_cutoff
-        self.last_rising_time = 0
-        self.last_falling_time = 0
-        self.debounce_counter = 0
-        self.state = False
+        # assign each of the CV outputs to specific duties
+        self.gate_out = cv1
+        self.incoming_rise_out = cv2
+        self.incoming_fall_out = cv3
+        self.gate_fall_out = cv4
+        self.toggle_out = cv5
+        self.toggle_fall_out = cv6
 
-        if not callable(cb_rising) or not callable(cb_falling):
-            raise ValueError("Provided callback func is not callable")
+        self.gate_on = False
+        self.toggle_on = False
 
-        self.cb_rising = cb_rising
-        self.cb_falling = cb_falling
+        # turn off all outputs initially
+        for cv in cvs:
+            cv.voltage(0)
 
-    def value(self):
-        """Returns europi.HIGH or europi.LOW depending on the state of the input
-        """
-        return HIGH if self.state else LOW
+        @din.handler
+        def on_din_rising():
+            self.on_rise()
 
-    def update(self):
-        """Reads the current value of the analogue input and updates the internal state
-        """
-        volts = self.ain.read_voltage()
+        @din.handler_falling
+        def on_din_falling():
+            self.on_fall()
 
-        # count how many opposite-voltage readings we have
-        if (self.state and volts < self.high_low_cutoff) or \
-           (not self.state and volts >= self.high_low_cutoff):
-            self.debounce_counter += 1
+        @b1.handler
+        def on_b1_press():
+            self.on_rise()
 
-        # change state if we've reached the debounce threshold
-        if self.debounce_counter >= self.debounce:
-            self.debounce_counter = 0
-            self.state = not self.state
-            if self.state:
-                self.last_rising_time = time.ticks_ms()
-                self.cb_rising()
-            else:
-                self.last_falling_time = time.ticks_ms()
-                self.cb_falling()
+        @b1.handler_falling
+        def on_b1_release():
+            self.on_fall()
 
-    def last_rising_ms(self):
-        return self.last_rising_time
-
-    def last_falling_ms(self):
-        return self.last_falling_time
-
-class GtWorker:
-    """Helper class for G&T
-
-    Handles rising & falling edges from the input/button and sets the output channels as needed
-    """
-
-    def __init__(self, gate_out, rise_out, fall_out, length_input):
-        """Constructor
-
-        @param gate_out  The CV output for the primary gate output
-        @param rise_out  The CV output for the rising-edge trigger
-        @param fall_out  The CV output for the falling-edge trigger
-        @param length_input  The analog input to control the length of the gate output
-        """
-
-        self.gate_out = gate_out
-        self.rise_out = rise_out
-        self.fall_out = fall_out
-        self.length_input = length_input
-
-        self.on_rise_start_time = 0
-        self.on_fall_start_time = 0
+        @b2.handler
+        def on_b2_press():
+            self.on_toggle()
 
     def on_rise(self):
         """Handle the rising edge of the input signal
         """
-        self.on_rise_start_time = time.ticks_ms()
-        self.rise_out.voltage(OUTPUT_VOLTAGE)
+        self.gate_on = True
+        self.on_incoming_rise_start_time = time.ticks_ms()
+        self.incoming_rise_out.voltage(OUTPUT_VOLTAGE)
         self.gate_out.voltage(OUTPUT_VOLTAGE)
+
+        self.on_toggle()
 
     def on_fall(self):
         """Handle the falling edge of the input signal
         """
-        self.on_fall_start_time = time.ticks_ms()
-        self.fall_out.voltage(OUTPUT_VOLTAGE)
+        self.on_incoming_fall_start_time = time.ticks_ms()
+        self.incoming_fall_out.voltage(OUTPUT_VOLTAGE)
+
+    def on_toggle(self):
+        """Handle toggling the toggle output
+        """
+        self.toggle_on = not self.toggle_on
+        if self.toggle_on:
+            self.toggle_out.voltage(OUTPUT_VOLTAGE)
+        else:
+            self.toggle_out.voltage(0)
+            self.toggle_fall_out.voltage(OUTPUT_VOLTAGE)
+            self.on_toggle_fall_start_time = time.ticks_ms()
 
     def tick(self):
         """Advance the clock and set the outputs high/low as needed
         """
         now = time.ticks_ms()
-        gate_duration = self.quadratic_knob(self.length_input.percent()*100)
+        knob_lvl = k1.percent()*100
+        cv_lvl = ain.percent() * k2.percent() * 2 * 100
+        gate_duration = self.quadratic_knob(cv_lvl + knob_lvl)
 
-        if time.ticks_diff(now, self.on_rise_start_time) > gate_duration:
+        if self.gate_on and time.ticks_diff(now, self.on_incoming_rise_start_time) > gate_duration:
+            self.gate_on = False
             self.gate_out.voltage(0)
+            self.gate_fall_out.voltage(OUTPUT_VOLTAGE)
+            self.on_gate_fall_start_time = now
 
-        if time.ticks_diff(now, self.on_rise_start_time) > TRIGGER_DURATION_MS:
-            self.rise_out.voltage(0)
+        if time.ticks_diff(now, self.on_gate_fall_start_time) > TRIGGER_DURATION_MS:
+            self.gate_fall_out.voltage(0)
 
-        if time.ticks_diff(now, self.on_fall_start_time) > TRIGGER_DURATION_MS:
-            self.fall_out.voltage(0)
+        if time.ticks_diff(now, self.on_incoming_rise_start_time) > TRIGGER_DURATION_MS:
+            self.incoming_rise_out.voltage(0)
+
+        if time.ticks_diff(now, self.on_incoming_fall_start_time) > TRIGGER_DURATION_MS:
+            self.incoming_fall_out.voltage(0)
+
+        if time.ticks_diff(now, self.on_toggle_fall_start_time) > TRIGGER_DURATION_MS:
+            self.toggle_fall_out.voltage(0)
 
     def quadratic_knob(self, x):
         """Some magic math to give us a quadratic response on the knob percentage
@@ -137,52 +123,16 @@ class GtWorker:
         This gives us 10ms @ 0% and 2000ms @ 100% with greater precision at the higher end
         where the differences will be more noticeable
         """
+        if x <= 0:
+            return 10
         return 199 * sqrt(x) + 10
-
-
-class GatesAndTriggers(EuroPiScript):
-    def __init__(self):
-        super().__init__()
-
-        self.channel1 = GtWorker(cv1, cv2, cv3, k1)
-        self.channel2 = GtWorker(cv4, cv5, cv6, k2)
-
-        @din.handler
-        def on_din_rising():
-            self.channel1.on_rise()
-
-        @din.handler_falling
-        def on_din_falling():
-            self.channel1.on_fall()
-
-        @b1.handler
-        def on_b1_press():
-            self.channel1.on_rise()
-
-        @b1.handler_falling
-        def on_b1_release():
-            self.channel1.on_fall()
-
-        @b2.handler
-        def on_b1_press():
-            self.channel2.on_rise()
-
-        @b2.handler_falling
-        def on_b1_release():
-            self.channel2.on_fall()
-
-        self.ain_wrapper = A2D_Wrapper(ain, \
-            cb_rising=lambda: self.channel2.on_rise(), \
-            cb_falling=lambda: self.channel2.on_fall())
 
     def main(self):
         # blank the screen since it's not needed
         oled.fill(0)
         oled.show()
         while(True):
-            self.ain_wrapper.update()
-            self.channel1.tick()
-            self.channel2.tick()
+            self.tick()
 
 
 if __name__=="__main__":
