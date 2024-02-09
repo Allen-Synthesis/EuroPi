22845f8eda3f139d32622210520e6f4d7ace4249
Rory Allen <79809962+roryjamesallen@users.noreply.github.com>
2024-02-09 17:39:56 +0000

    use max output voltage from config

--- software/contrib/harmonic_lfos.py
+++ software/contrib/harmonic_lfos.py
@@ -5,7 +5,7 @@ from machine import freq
 from random import randint
 from europi_script import EuroPiScript
 
-MAX_VOLTAGE = MAX_OUTPUT_VOLTAGE # Default is inherited but this can be overriden by replacing "MAX_OUTPUT_VOLTAGE" with an integer
+MAX_VOLTAGE = europi_config["max_output_voltage"] # Default is inherited but this can be overriden by replacing "MAX_OUTPUT_VOLTAGE" with an integer
 MAX_HARMONIC = 32 # Too high a value may be hard to select using the knob, but the actual hardware limit is only reached at 4096
 
 
