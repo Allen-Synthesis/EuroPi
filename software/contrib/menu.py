1952261cccf663e3feb775f9b0ff6dd07009f525
Rory Allen <79809962+roryjamesallen@users.noreply.github.com>
2024-02-13 19:47:20 +0000

    remove usb workaround (#333)
    Workaround added in #183, removal conditions set out in #184 appear to have been met, and using MicroPython 1.20.0 or greater this issue no longer occurs

--- software/contrib/menu.py
+++ software/contrib/menu.py
@@ -2,11 +2,6 @@
 # Reset the module state and display bootsplash screen.
 from europi import bootsplash, usb_connected
 
-#  This is a fix for a USB connection issue documented in GitHub issue #179, and its removal condition is set out in GitHub issue #184
-if usb_connected.value() == 0:
-    from time import sleep
-
-    sleep(0.5)
 
 bootsplash()
 
