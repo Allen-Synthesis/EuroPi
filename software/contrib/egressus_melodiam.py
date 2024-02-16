90c1382cff4f33877948a373c93dcdaa070c7d29
gamecat69 <gamecat69@hotmail.com>
2024-02-16 22:08:10 +0000

    removed overclocking - it is already done in the firmware

--- software/contrib/egressus_melodiam.py
+++ software/contrib/egressus_melodiam.py
@@ -102,9 +102,6 @@ KNOB_CHANGE_TOLERANCE = 0.999
 # Important: Needs firmware v0.12.1 or higher
 MAX_CV_VOLTAGE = europi_config["max_output_voltage"]
 
-# Set the desired CPU frequency using a global config value. Default is overclocked.
-CPU_FREQ = europi_config["cpu_freq"]
-
 MAX_STEP_LENGTH = 32
 
 # Diff between incoming clocks are stored in the FiFo buffer and averaged
@@ -147,9 +144,6 @@ WAVE_SHAPE_IMGS = [
 
 class EgressusMelodiam(EuroPiScript):
     def __init__(self):
-
-        # Overclock the Pico for improved performance if configured globally
-        machine.freq(CPU_FREQ)
 
         # Initialize variables
         self.newClockToProcess = False
