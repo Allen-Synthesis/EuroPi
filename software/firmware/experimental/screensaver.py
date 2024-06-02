9362eca27cdbb27e117a40c004bbafc1ecb8853e
Chris I-B <ve4cib@gmail.com>
2024-06-02 09:30:10 -0400

    Reduce latency in Clock Modifer script (#363)
    * Use microseconds for calculating the timing instead of milliseconds. Keep track of when the GUI needs re-rendering instead of always redrawing to reduce latency
    
    * Simplify state calculation
    
    * Minor linting
    
    * Increase the timeout for the incoming clock to 5s (from 1s). Re-implement the clock multiplication & division to force it to re-sync to the incoming gates. Division is now implemented as a simple gate-skipper. Multiplication phase times are based on the last incoming gate time instead of a free-running counter
    
    * Add an upper bound to the gate counter to prevent overflows/performance degredation with large integers
    
    * Add a screensaver since the UI is otherwise pretty static. Should prevent burn-in when used for extended periods
    
    * Fix the order of the screensaver & blank conditions so they actually fire correctly
    
    * Just remove the blanking; it always forces a re-render, which can impact the timing
    
    * Fix a bug where the screensaver can appear stuck when time.ticks_diff wraps around during a notably long interval
    
    * Prevent ticks_diff from wrapping around too far by synthetically incrementing the last-render time

--- software/firmware/experimental/screensaver.py
+++ software/firmware/experimental/screensaver.py
@@ -30,9 +30,11 @@ class Screensaver:
 
     ## Standard duration before we activate the screensaver
     ACTIVATE_TIMEOUT_MS = 1000 * 60 * 5
+    ACTIVATE_TIMEOUT_US = ACTIVATE_TIMEOUT_MS * 1000
 
     ## Standard duration before we blank the screen
     BLANK_TIMEOUT_MS = 1000 * 60 * 20
+    BLANK_TIMEOUT_US = BLANK_TIMEOUT_MS * 1000
 
     def __init__(self):
         self.last_logo_reposition_at = 0
@@ -49,7 +51,8 @@ class Screensaver:
         LOGO_UPDATE_INTERVAL = 2000
 
         now = utime.ticks_ms()
-        if force or time.ticks_diff(now, self.last_logo_reposition_at) > LOGO_UPDATE_INTERVAL:
+        elapsed_ms = time.ticks_diff(now, self.last_logo_reposition_at)
+        if force or abs(elapsed_ms) >= LOGO_UPDATE_INTERVAL:
             self.last_logo_reposition_at = now
             x = random.randint(0, OLED_WIDTH - self.LOGO_WIDTH)
             y = random.randint(0, OLED_HEIGHT - self.LOGO_HEIGHT)
