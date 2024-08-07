9520e458843bedc70cf5ec4b3a399cce47118673
Chris I-B <ve4cib@gmail.com>
2024-08-07 19:22:50 -0400

    Copy the entirety of bisect from the source; it's not that big and we might as well include all of it for convenience

--- software/firmware/experimental/bisect.py
+++ software/firmware/experimental/bisect.py
@@ -1,9 +1,83 @@
 #!/usr/bin/env python3
 """
-A replacement for the standard bisect Python library
+Functions for searching, splitting, and inserting into a sorted list
 
-Currently only supports a subset of the full module
+Copied from https://github.com/python/cpython/blob/3.12/Lib/bisect.py with some modifications
+noted in-line with `# EDIT`
 """
+
+
+def insort_right(a, x, lo=0, hi=None, *, key=None):
+    """Insert item x in list a, and keep it sorted assuming a is sorted.
+
+    If x is already in a, insert it to the right of the rightmost x.
+
+    Optional args lo (default 0) and hi (default len(a)) bound the
+    slice of a to be searched.
+
+    A custom key function can be supplied to customize the sort order.
+    """
+    if key is None:
+        lo = bisect_right(a, x, lo, hi)
+    else:
+        lo = bisect_right(a, key(x), lo, hi, key=key)
+    a.insert(lo, x)
+
+
+def bisect_right(a, x, lo=0, hi=None, *, key=None):
+    """Return the index where to insert item x in list a, assuming a is sorted.
+
+    The return value i is such that all e in a[:i] have e <= x, and all e in
+    a[i:] have e > x.  So if x already appears in the list, a.insert(i, x) will
+    insert just after the rightmost x already there.
+
+    Optional args lo (default 0) and hi (default len(a)) bound the
+    slice of a to be searched.
+
+    A custom key function can be supplied to customize the sort order.
+    """
+
+    if lo < 0:
+        # EDIT
+        #raise ValueError('lo must be non-negative')
+        lo = 0
+    if hi is None:
+        hi = len(a)
+    # Note, the comparison uses "<" to match the
+    # __lt__() logic in list.sort() and in heapq.
+    if key is None:
+        while lo < hi:
+            mid = (lo + hi) // 2
+            if x < a[mid]:
+                hi = mid
+            else:
+                lo = mid + 1
+    else:
+        while lo < hi:
+            mid = (lo + hi) // 2
+            if x < key(a[mid]):
+                hi = mid
+            else:
+                lo = mid + 1
+    return lo
+
+
+def insort_left(a, x, lo=0, hi=None, *, key=None):
+    """Insert item x in list a, and keep it sorted assuming a is sorted.
+
+    If x is already in a, insert it to the left of the leftmost x.
+
+    Optional args lo (default 0) and hi (default len(a)) bound the
+    slice of a to be searched.
+
+    A custom key function can be supplied to customize the sort order.
+    """
+
+    if key is None:
+        lo = bisect_left(a, x, lo, hi)
+    else:
+        lo = bisect_left(a, key(x), lo, hi, key=key)
+    a.insert(lo, x)
 
 
 def bisect_left(a, x, lo=0, hi=None, *, key=None):
@@ -17,13 +91,10 @@ def bisect_left(a, x, lo=0, hi=None, *, key=None):
     slice of a to be searched.
 
     A custom key function can be supplied to customize the sort order.
-
-    Copied from https://github.com/python/cpython/blob/3.12/Lib/bisect.py with some modifications
-    noted below
     """
 
     if lo < 0:
-        # EDIT: force lo to be non-zero to avoid raising a potentially unhandled exception
+        # EDIT
         # raise ValueError('lo must be non-negative')
         lo = 0
     if hi is None:
@@ -45,3 +116,8 @@ def bisect_left(a, x, lo=0, hi=None, *, key=None):
             else:
                 hi = mid
     return lo
+
+
+# Create aliases
+bisect = bisect_right
+insort = insort_right
