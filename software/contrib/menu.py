4ba118d2bf3cc002d463465719b6c7b32cb1521d
Chris I-B <ve4cib@gmail.com>
2024-11-11 16:13:13 -0500

    Add tools.about for easily checking the version, CPU clocking, & configured EuroPi & Pico models

--- software/contrib/menu.py
+++ software/contrib/menu.py
@@ -63,6 +63,7 @@ EUROPI_SCRIPTS = OrderedDict([
     ["Turing Machine",    "contrib.turing_machine.EuroPiTuringMachine"],
     ["Volts",             "contrib.volts.OffsetVoltages"],
 
+    ["_About",            "tools.about.About"],                # this one should always be fourth to last!
     ["_Diagnostic",       "tools.diagnostic.Diagnostic"],      # this one should always be third to last!
     ["_Calibrate",        "tools.calibrate.Calibrate"],        # this one should always be second to last!
     ["_BootloaderMode",   "bootloader_mode.BootloaderMode"]    # this one should always be last!
