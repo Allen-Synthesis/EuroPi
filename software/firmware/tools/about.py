0e8a634ab6a47681b57555c1f4e5786526929a8c
Chris I-B <ve4cib@gmail.com>
2024-11-11 16:19:09 -0500

    Linting

--- software/firmware/tools/about.py
+++ software/firmware/tools/about.py
@@ -3,6 +3,7 @@ from europi_script import EuroPiScript
 from time import sleep
 from version import __version__
 
+
 class About(EuroPiScript):
     def __init__(self):
         super().__init__()
@@ -12,10 +13,12 @@ class About(EuroPiScript):
 
         oled.centre_text(f"""EuroPi v{__version__}
 {europi_config.EUROPI_MODEL}/{europi_config.PICO_MODEL}
-{europi_config.CPU_FREQ}""")
+{europi_config.CPU_FREQ}"""
+        )
 
         while True:
             sleep(1)
+
 
 if __name__ == "__main__":
     About().main()
