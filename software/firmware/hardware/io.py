52175d819cfc4ae38575e3f3c01ec95015e901d2
Chris I-B <ve4cib@gmail.com>
2024-11-11 23:33:07 -0500

    Linting

--- software/firmware/hardware/io.py
+++ software/firmware/hardware/io.py
@@ -22,7 +22,11 @@ class AnalogueReader:
     not need to be used by user scripts.
     """
 
-    def __init__(self, pin, samples=DEFAULT_SAMPLES, deadzone=0.0):
+    def __init__(self,
+        pin,
+        samples=DEFAULT_SAMPLES,
+        deadzone=0.0
+    ):
         self.pin_id = pin
         self.pin = ADC(Pin(pin))
         self.set_samples(samples)
