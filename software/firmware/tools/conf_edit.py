5079fdbadbb78c4ac0d89d69f4f5877ee94e54f0
Chris I-B <ve4cib@gmail.com>
2024-12-05 01:27:01 -0500

    Separate the settings into sections, process the config point name to do our best to fit most of them on-screen

--- software/firmware/tools/conf_edit.py
+++ software/firmware/tools/conf_edit.py
@@ -13,23 +13,99 @@ from configuration import ConfigFile
 from experimental.settings_menu import *
 
 
+class SectionHeader(MenuItem):
+    def __init__(self, title, children):
+        super().__init__(children=children)
+        self.title = title
+
+    def draw(self, oled):
+        oled.centre_text(f"{self.title}\n\nHold B2 to edit")
+
+
 class ConfigurationEditor(EuroPiScript):
     def __init__(self):
         super().__init__()
 
+        boolean_labels = {
+            True: "Yes",
+            False: "No"
+        }
+
         # fmt: off
+        voltage_items = []
+        system_items = []
+        display_items = []
+        i2c_items = []
+
         config_points = EuroPiConfig.config_points()
-        menu_items = []
         for cfg in config_points:
-            menu_items.append(
-                SettingMenuItem(
-                    config_point=cfg,
-                    float_resolution=1
+            if "EXTERNAL_I2C" in cfg.name:
+                i2c_items.append(
+                    SettingMenuItem(
+                        config_point=cfg,
+                        prefix="I2C",
+                        title=cfg.name.replace("EXTERNAL_I2C", "").replace("_", ' ').lower().strip(),
+                        float_resolution=1,
+                        labels=boolean_labels
+                    )
                 )
-            )
+            elif "DISPLAY" in cfg.name:
+                display_items.append(
+                    SettingMenuItem(
+                        config_point=cfg,
+                        prefix="Display",
+                        title=cfg.name.replace("DISPLAY", "").replace("_", ' ').lower().strip(),
+                        float_resolution=1,
+                        labels=boolean_labels
+                    )
+                )
+            elif "VOLTAGE" in cfg.name:
+                voltage_items.append(
+                    SettingMenuItem(
+                        config_point=cfg,
+                        prefix="Voltage",
+                        title=cfg.name.replace("VOLTAGE", "").replace("_", ' ').lower().replace("input", "in").replace("output", "out").strip(),
+                        float_resolution=1,
+                        labels=boolean_labels
+                    )
+                )
+            else:
+                system_items.append(
+                    SettingMenuItem(
+                        config_point=cfg,
+                        prefix="Sys",
+                        title=cfg.name.replace("_", ' ').lower().strip(),
+                        float_resolution=1,
+                        labels=boolean_labels
+                    )
+                )
 
         self.menu = SettingsMenu(
-            menu_items=menu_items
+            menu_items=[
+                # Voltage properties
+                SectionHeader(
+                    title="Voltage",
+                    children=voltage_items
+                ),
+
+                # System properties
+                SectionHeader(
+                    title="System",
+                    children=system_items
+                ),
+
+                # Display properties
+                SectionHeader(
+                    title="Display",
+                    children=display_items
+                ),
+
+                # External I2C properties
+                SectionHeader(
+                    title="I2C",
+                    children=i2c_items
+                ),
+            ]
         )
         self.menu.load_defaults(ConfigFile.config_filename(EuroPiConfig))
         # fmt: on
