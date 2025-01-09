8fcafa98fe3c8babe19a7aa9492153bbd1a1537f
Chris I-B <ve4cib@gmail.com>
2025-01-09 18:53:57 -0500

    Add Itty Bitty script (#398)

--- software/contrib/menu.py
+++ software/contrib/menu.py
@@ -38,6 +38,7 @@ EUROPI_SCRIPTS = OrderedDict([
     ["Hamlet",            "contrib.hamlet.Hamlet"],
     ["HarmonicLFOs",      "contrib.harmonic_lfos.HarmonicLFOs"],
     ["HelloWorld",        "contrib.hello_world.HelloWorld"],
+    ["Itty Bitty",        "contrib.itty_bitty.IttyBitty"],
     ["KnobPlayground",    "contrib.knob_playground.KnobPlayground"],
     ["Kompari",           "contrib.kompari.Kompari"],
     ["Logic",             "contrib.logic.Logic"],
