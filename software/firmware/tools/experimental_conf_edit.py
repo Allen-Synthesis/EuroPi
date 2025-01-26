748cdc937a1d752e655b5281a5ecba6900670ab7
Chris I-B <ve4cib@gmail.com>
2025-01-26 17:40:15 -0500

    Use lockable knobs for the configuration editors

--- software/firmware/tools/experimental_conf_edit.py
+++ software/firmware/tools/experimental_conf_edit.py
@@ -11,7 +11,21 @@ from europi_script import EuroPiScript
 from configuration import ConfigFile
 
 from experimental.experimental_config import *
+from experimental.knobs import KnobBank
 from experimental.settings_menu import *
+
+
+## Lockable knob bank for K2 to make menu navigation a little easier
+#
+#  Note that this does mean _sometimes_ you'll need to sweep the knob all the way left/right
+#  to unlock it
+k2_bank = (
+    KnobBank.builder(k2)
+    .with_unlocked_knob("main_menu")
+    .with_locked_knob("submenu", initial_percentage_value=0)
+    .with_locked_knob("choice", initial_percentage_value=0)
+    .build()
+)
 
 
 class ExperimentalConfigurationEditor(EuroPiScript):
@@ -47,7 +61,8 @@ class ExperimentalConfigurationEditor(EuroPiScript):
             )
 
         self.menu = SettingsMenu(
-            menu_items=items
+            menu_items=items,
+            navigation_knob=k2_bank,
         )
         self.menu.load_defaults(ConfigFile.config_filename(ExperimentalConfig))
         # fmt: on
