0e522abe049c24397a359d132a2092a73f0f6dd2
Chris I-B <ve4cib@gmail.com>
2025-02-14 00:05:49 -0500

    Start implementing a basic HTTP server and a server-based control script

---
+++ software/contrib/http_control.py
@@ -0,0 +1,42 @@
+# Copyright 2025 Allen Synthesis
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+"""
+Serves a simple HTTP page to control the levels of the six CV outputs from a browser
+
+Requires a Pico W or Pico 2 W with a valid wifi setup to work
+"""
+
+from europi import *
+from europi_script import EuroPiScript
+
+from experimental.http import *
+
+class HttpControl(EuroPiScript):
+
+    def __init__(self):
+        super().__init__()
+
+        self.server = HttpServer(80)
+
+        @self.server.request_handler
+        def handle_request(connection=None, request=None):
+            raise NotImplementedError("WIP - Not implemented yet")
+
+    def main(self):
+        while True:
+            self.server.check_requests()
+
+
+if __name__ == "__main__":
+    HttpControl().main()
---
+++ software/firmware/experimental/http_server.py
@@ -0,0 +1,165 @@
+# Copyright 2025 Allen Synthesis
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+"""
+A simple HTTP server for the Raspberry Pi Pico
+"""
+
+try:
+    import socket
+except ImportError as err:
+    raise Exception(f"Failed to load HTTP server dependencies: {err}")
+
+
+class HttpStatus:
+    """
+    HTTP status codes to include in the header.
+
+    This collection is not exhaustive, just what we need to handle this minimal server implementation
+    """
+    OK = "200 OK"
+
+    BAD_REQUEST = "400 Bad Request"
+    FORBIDDEN = "403 Forbidden"
+    NOT_FOUND = "404 Not Found"
+    TEAPOT = "418 I'm a teapot"  # for fun & debugging!
+
+    INTERNAL_SERVER_ERROR = "500 Internal Server Error"
+    NOT_IMPLEMENTED = "501 Not Implemented"
+
+class MimeTypes:
+    """
+    Common MIME types we can support with this HTTP server implementation
+    """
+    CSV = "text/csv"
+    HTML = "text/html"
+    JSON = "text/json"
+    TEXT = "text/plain"
+    XML = "text/xml"
+    YAML = "text/yaml"
+
+
+class HttpServer:
+    """
+    The main server instance
+
+    You should define a callback to handle incoming requests, e.g.:
+
+    server = HttpServer(port=8080)
+
+    @server.request_handler
+    def handle_http_request(request:str=None, conn:Socket=None):
+        # process the request
+        server.send_response()
+    """
+
+    # A basic error page template
+    ERROR_PAGE = """<!DOCTYPE html>
+<html>
+  <head>
+    <title>EuroPi Error: {title}</title>
+  </head>
+  <body>
+    <h1>EuroPi Error</h1>
+    <h2>{title}</h2>
+    <p>{body}</p>
+  </body>
+</html>
+"""
+
+    def __init__(self, port=80):
+        self.port = 80
+
+        self.request_callback = lambda *args, **kwargs: None
+
+        if wifi_connection is None:
+            raise WifiError("Unable to start HTTP server: no wifi connection")
+
+        self.socket = socket.socket()
+        addr = socket.getaddrinfo('0.0.0.0', port)[0][-1]
+        s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        s.bind(addr)
+        s.listen()
+
+    def check_requests(self):
+        """
+        Poll the socket and process any incoming requests.
+
+        This will invoke the request handler function if it has been defined
+
+        This function should be called inside the main loop of the program
+        """
+        try:
+            conn, addr = self.socket.accept()
+
+            request = conn.recv(2048)
+            request = str(request=request, connection=conn)
+
+            self.request_callback(request)
+
+        except OSError as err:
+            print(f"Connection closed: {err}")
+        except NotImplementedError as err:
+            self.send_response(
+                conn,
+                self.ERROR_PAGE.format(
+                    title=HttpStatus.NOT_IMPLEMENTED,
+                    body=str(err),
+                ),
+                status=HttpStatus.NOT_IMPLEMENTED,
+                content_type="text/html"
+            )
+        except Exception as err:
+            self.send_response(
+                conn,
+                self.ERROR_PAGE.format(
+                    title=HttpStatus.INTERNAL_SERVER_ERROR,
+                    body=str(err),
+                ),
+                status=HttpStatus.INTERNAL_SERVER_ERROR,
+                content_type="text/html"
+            )
+
+    def request_handler(self, func):
+        """
+        Decorator for the function to handle HTTP requests
+
+        The provided function must accept the following keyword arguments:
+        - request: str  The request the client sent
+        - conn: socket  A socket connection to the client
+
+        @param func  The function to handle the request.
+        """
+        def wrapper(*args, **kwargs):
+            func(*args, **kwargs)
+
+        self.request_callback = wrapper
+        return wrapper
+
+    def send_response(
+        self,
+        connection:socket.socket,
+        response:str,
+        status:str=HttpStatus.OK,
+        content_type:str=MimeTypes.HTML,
+    ):
+        """
+        Send a response to the client
+
+        @param connection  The socket connection to the client
+        @param response  The response payload
+        @param content_type  The MIME type to include in the HTTP header
+        """
+        connection.send(f"HTTP/1.0 200 OK\r\nContent-type: {content_type}\r\n\r\n")
+        connection.send(response)
+        connection.close()
