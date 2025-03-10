997fc940c9bdde930016ed28156f21e69f692020
Chris I-B <ve4cib@gmail.com>
2025-03-10 11:46:44 -0400

    Remove TODO items from mock network module, add additional constants & functions

--- software/tests/mocks/network.py
+++ software/tests/mocks/network.py
@@ -11,6 +11,67 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+"""
+Mock implementation of Micropython's network module
+
+This module is only present on the Pico W and Pico 2 W; on Pico variations
+without wireless modules it cannot be imported.
+"""
+
+STAT_WRONG_PASSWORD = -3
+STAT_NO_AP_FOUND = -2
+STAT_CONNECT_FAIL = -1
+STAT_IDLE = 0
+STAT_CONNECTING = 1
+STAT_GOT_IP = 3
+
+
+def country(code=None):
+    """
+    Get/set the country code.
+
+    @param code  The ISO-compatible 2-character country code
+    @return The ISO-compatible 2-character country code, or XX if unset
+    """
+    if code:
+        return code
+    else:
+        return "XX"
+
+
+def hostname(name=None):
+    """
+    Get/set the hostname.
+
+    @param name  The new hostname to assign
+    @return This device's hostname
+    """
+    if name:
+        return name
+    else:
+        return "PicoW"
+
+
+def ipconfig(**kwargs):
+    """
+    Get/set global IP paramters.
+
+    @param kwargs  Keyword arguments to assign: dns, prefer
+    @return The value of the parameter
+    """
+    return None
+
+
+def route():
+    """
+    Get static route information.
+
+    This doesn't appear to be documented on
+    https://docs.micropython.org/en/latest/library/network.html
+    but exists on the Pico 2 W, returning an empty list when
+    called. So that behaviour is replicated here.
+    """
+    return []
 
 
 class WIFI:
@@ -20,14 +81,16 @@ class WIFI:
     See: https://docs.micropython.org/en/latest/library/network.WLAN.html
     """
 
-    # TODO: are these correct?
     IF_STA = 0
     IF_AP = 1
 
-    # TODO: are these correct?
-    PM_PERFORMANCE = 0
-    PM_POWERSAVE = 1
-    PM_NONE = 2
+    SEC_OPEN = 0
+    SEC_WPA_WPA2 = 4194310
+
+    PM_NONE = 16
+    PM_POWERSAVE = 17
+    PM_PERFORMANCE = 10555714
+
 
     def __init__(self, mode):
         pass
