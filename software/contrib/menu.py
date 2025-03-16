7f7f28b93a22b7cfd416f19b6f0970e28ce09e4a
Chris I-B <ve4cib@gmail.com>
2025-03-16 15:22:04 -0400

    Rename HTTP Control -> HTTP Interface in menu
    Keeps consistency with OSC Interface

--- software/contrib/menu.py
+++ software/contrib/menu.py
@@ -53,7 +53,7 @@ EUROPI_SCRIPTS = OrderedDict([
     ["Gate Phaser",       "contrib.gate_phaser.GatePhaser"],
     ["Hamlet",            "contrib.hamlet.Hamlet"],
     ["HarmonicLFOs",      "contrib.harmonic_lfos.HarmonicLFOs"],
-    ["HTTP Control",      "contrib.http_control.HttpControl"],
+    ["HTTP Interface",    "contrib.http_control.HttpControl"],
     ["Itty Bitty",        "contrib.itty_bitty.IttyBitty"],
     ["Kompari",           "contrib.kompari.Kompari"],
     ["Logic",             "contrib.logic.Logic"],
