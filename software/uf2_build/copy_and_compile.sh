586655f78942dae9bf604f24fa18554ebf3be3d9
Chris I-B <ve4cib@gmail.com>
2025-03-24 23:44:59 -0400

    Add additional steps to build UF2 files for all available boards. Note: Pico 2 W is _not_ available yet, so hold off merging this until it is

--- software/uf2_build/copy_and_compile.sh
+++ software/uf2_build/copy_and_compile.sh
@@ -1,18 +1,23 @@
-#/bin/sh
+#/bin/bash
 set -e
 
 echo "Copying EuroPi firmware and scripts to container..."
-mkdir micropython/ports/rp2/modules/contrib
-mkdir micropython/ports/rp2/modules/experimental
-mkdir micropython/ports/rp2/modules/tools
-cp -r europi/software/firmware/*.py micropython/ports/rp2/modules
-cp -r europi/software/firmware/experimental/*.py micropython/ports/rp2/modules/experimental
-cp -r europi/software/firmware/tools/*.py micropython/ports/rp2/modules/tools
-cp -r europi/software/contrib/*.py micropython/ports/rp2/modules/contrib
+mkdir /micropython/ports/rp2/modules/contrib
+mkdir /micropython/ports/rp2/modules/experimental
+mkdir /micropython/ports/rp2/modules/tools
+cp -r europi/software/firmware/*.py /micropython/ports/rp2/modules
+cp -r europi/software/firmware/experimental/*.py /micropython/ports/rp2/modules/experimental
+cp -r europi/software/firmware/tools/*.py /micropython/ports/rp2/modules/tools
+cp -r europi/software/contrib/*.py /micropython/ports/rp2/modules/contrib
 
 echo "Compiling micropython and firmware modules..."
-cd micropython/ports/rp2
-make
+cd /micropython/ports/rp2
 
-echo "Copying firmware file to /europi/software/uf2_build/europi-dev.uf2"
-cp build-RPI_PICO/firmware.uf2 /europi/software/uf2_build/europi-dev.uf2
+BOARDS="RPI_PICO RPI_PICO2 RPI_PICO_W RPI_PICO2_W"
+
+for b in $BOARDS; do
+    echo "make BOARD=$b"
+    make BOARD=$b
+    echo "Moving firmware file to /europi/software/uf2_build/europi-$b-dev.uf2"
+    mv build-$b/firmware.uf2 /europi/software/uf2_build/europi-$b-dev.uf2
+done
