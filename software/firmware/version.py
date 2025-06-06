a14a459e2ad78e03117d390f77ad933f1ab430fb
Chris I-B <ve4cib@gmail.com>
2025-06-05 20:38:43 -0400

    Bump version

--- software/firmware/version.py
+++ software/firmware/version.py
@@ -12,4 +12,4 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 """EuroPi firmware and contrib package version"""
-__version__ = "0.20.0"
+__version__ = "0.21.0"
